679fd3d4d2f808f9453c030d0bb316cf3b007b3e
itstaff1027 <itstaffdelrosario@gmail.com>
2025-01-29 12:09:51 +0800

    Add: My own ip address to access login

--- app/Http/Middleware/IpWhitelist.php
+++ app/Http/Middleware/IpWhitelist.php
@@ -17,11 +17,12 @@ class IpWhitelist
      */
 
      protected $allowedIps = [
-        '192.168.254.*', // If your device is in this range
+        // '192.168.254.*', // If your device is in this range
         // '192.168.1.*',   // Add this if your device is here
         // '10.0.*.*',      // Another local range
         // '203.0.113.45',  // Public IP of your Wi-Fi router
         // '127.0.0.1',     // Localhost
+        '180.191.166.35'
     ];
     /**
      * Handle an incoming request.
