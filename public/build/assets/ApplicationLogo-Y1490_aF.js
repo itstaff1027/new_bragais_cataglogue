f9e9aa1b6ace139b68fd0d74c50ed9032d34cf40
itstaff1027 <itstaffdelrosario@gmail.com>
2025-01-30 13:32:36 +0800

    Testing: For production build if images will work now'

--- public/build/assets/ApplicationLogo-Y1490_aF.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-BoLXcIFP.js";function p({src:o,alt:t="Logo",...i}){return r.jsx("img",{src:o,alt:t,...i})}export{p as A};
---
+++ public/build/assets/ApplicationLogo-_-crNB77.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-CJ7IyaXc.js";function p({src:o,alt:t="Logo",...i}){return r.jsx("img",{src:o,alt:t,...i})}export{p as A};
